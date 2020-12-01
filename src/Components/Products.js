ea5ffbf9bd7f5a4bce5d25a84f312a4a5dc15737
RivaD2 <rivkadavidowski@gmail.com>
2020-12-01 12:18:11 -0800

    added food card and added titles to all card images

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -8,6 +8,8 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import beauty from '../assets/beauty.jpeg'
+import home from '../assets/home-decor.jpg'
+import food from '../assets/food.jpeg';
 
 const useStyles = makeStyles({
   root: {
@@ -21,7 +23,8 @@ const useStyles = makeStyles({
 export default function MediaCard() {
   const classes = useStyles();
 
-  return (
+  return ( 
+    <>
     <Card className={classes.root}>
       <CardActionArea>
         <CardMedia
@@ -34,7 +37,7 @@ export default function MediaCard() {
             Beauty
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            Enter Text Here
+            Check out the latest in Beauty
           </Typography>
         </CardContent>
       </CardActionArea>
@@ -47,5 +50,56 @@ export default function MediaCard() {
         </Button>
       </CardActions>
     </Card>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          title="home-decor"
+          image={home}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Home-Decor
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Up your comfort
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+         Add to Cart
+        </Button>
+        <Button size="small" color="primary">
+          View Details
+        </Button>
+      </CardActions>
+    </Card>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          title="food"
+          image={food}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Food
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+           Best recipes 2020
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+         Add to Cart
+        </Button>
+        <Button size="small" color="primary">
+          View Details
+        </Button>
+      </CardActions>
+    </Card>
+    </>
   );
 }
